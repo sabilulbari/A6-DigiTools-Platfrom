6e16a14e8fd9bb9895e92a0f4f92e5b28ab20709
sabilulbari <mdsabilulbari100@gmail.com>
2026-03-30 23:39:16 +0600

    Card design fixed

--- src/components/home/pricing-card/card/card.jsx
+++ src/components/home/pricing-card/card/card.jsx
@@ -2,13 +2,21 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-sm">
+    <div className="w-[90%] mx-auto">
+      <div className="card w-96 bg-base-100 border border-gray-200 shadow-sm">
         <div className="card-body">
-          <span className="badge badge-xs badge-warning">Most Popular</span>
-          <div className="flex justify-between">
+          <h3 className="bg-amber-200  text-right">Most Popular</h3>
+          <div className="flex  mb-4">
+            <div className="w-10 h-10 border-2 border-amber-600 rounded-full flex items-center justify-center bg-amber-50">
+              <img className="w-8 h-8 rounded-full object-cover" src="/src/assets/writing_2327400 1.png" alt="logo" />
+            </div>
+          </div>
+          <div className="">
             <h2 className="text-3xl font-bold">Premium</h2>
-            <span className="text-xl">$29/mo</span>
+            <p className="font-normal text-[16] text-[#627382]">Generate high-quality content, blogs, and marketing copy in seconds with advanced AI.</p>
+            <span className="text-md flex justify-start">
+              <span className="text-xl">$29/</span>mo
+            </span>
           </div>
           <ul className="mt-6 flex flex-col gap-2 text-xs">
             <li>
@@ -19,7 +27,7 @@ const Card = () => {
             </li>
           </ul>
           <div className="mt-6">
-            <button className="btn btn-primary btn-block">Subscribe</button>
+            <button className="btn w-full rounded-full text-white bg-linear-to-r from-indigo-600 to-violet-500">Subscribe</button>
           </div>
         </div>
       </div>
