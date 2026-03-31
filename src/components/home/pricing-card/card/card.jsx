f83de7c50c54db5537e7502ad7244ff40ceb738c
sabilulbari <mdsabilulbari100@gmail.com>
2026-03-31 12:21:17 +0600

    Feature part problem fix

--- src/components/home/pricing-card/card/card.jsx
+++ src/components/home/pricing-card/card/card.jsx
@@ -24,7 +24,7 @@ const Card = ({ card }) => {
           <ul className="mt-6 flex flex-col gap-2 text-xs">
             {features.map((feture) => 
             {
-              <div>
+              return <div>
                 <li>
                   <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
